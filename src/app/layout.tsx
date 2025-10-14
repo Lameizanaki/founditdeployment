49621656b9d1e267842ff3270b84cfac396f6af5
vattana.puth <puthvattanangeen@gmail.com>
2025-10-14 19:57:53 +0700

    init_projects_structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Inter, Roboto, Andada_Pro} from "next/font/google";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const roboto = Roboto({ subsets: ["latin"], variable: "--font-roboto"});
+const andada = Andada_Pro({ subsets: ["latin"], variable: "--font-andada"});
+const geistSans = Geist({variable: "--font-geist-sans", subsets: ["latin"],});
+const geistMono = Geist_Mono({variable: "--font-geist-mono",subsets: ["latin"],});
+
+export const metadata: Metadata = {
+  title: "www.founded.com",
+  description: "'Founded' platform unites clients, freelancers, and sellers in one workspace. Post jobs, get proposals, and track milestones. Freelancers showcase portfolios, win contracts, and get paid via secure escrow. Sellers list digital goods and services with instant delivery. Built-in chat, ratings, and disputes keep work safe and simple.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} ${roboto.variable} ${andada.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
