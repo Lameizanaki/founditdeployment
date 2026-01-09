d6b3b4320bb2cb0f8dcf2444c570a8fb2b5242d2
Vattana <puthvattanangeen@gmail.com>
2026-01-10 00:55:28 +0700

    merge_main1_2

---
+++ src/app/page/freelancer/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from 'react';
+import FreelancerNavHeader from '@/app/components/styles/global_styles/freelancer/header';
+import FreelancerFooter from '@/app/components/styles/global_styles/freelancer/footer';
+
+export default function FreelancerPage() {
+  return (
+    <div>
+        <FreelancerNavHeader />
+        <FreelancerFooter />
+    </div>
+  );
+}
