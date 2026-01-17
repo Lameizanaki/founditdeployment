e4ee598b8f279b091cd3a0219a28339b4864de2a
lyheng <hengizanaki@gmail.com>
2026-01-17 13:14:52 +0700

    deploy1

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // ✅ allow build even if ESLint has errors (temporary for hosting)
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
   async rewrites() {
     return [
       {
